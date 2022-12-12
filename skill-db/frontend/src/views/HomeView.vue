fe58e5bd1749de4d858e0635943f66ea8e29f390
SaskiaAsmussen <saskia.asmussen@cofinpro.de>
2022-12-12 07:34:21 +0100

    [FEATURE] Added vuetify to frontend

--- skill-db/frontend/src/views/HomeView.vue
+++ skill-db/frontend/src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
     <TheWelcome />
   </main>
+  <v-chip>Test 123</v-chip>
 </template>
+
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
