3d36df1a3b1667fe2cd3c212b929b5b83614c391
MarkusKremer <Markus.Kremer@cofinpro.de>
2022-12-12 09:38:55 +0100

    [INITIALIZE] Delete HelloWorld Components, start overview

--- skill-db/frontend/src/views/HomeView.vue
+++ skill-db/frontend/src/views/HomeView.vue
@@ -1,10 +1,31 @@
 <template>
-  <main>
-    <TheWelcome />
-  </main>
-  <v-chip>Test 123</v-chip>
+  <v-app id="inspire">
+    <v-app-bar>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container >
+        <v-row >
+          <v-col v-for="n in 22" :key="n" cols="4" elevation="5">
+            <v-card height="200"></v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<script lang="ts">
+export default {
+  //
+}
 </script>
