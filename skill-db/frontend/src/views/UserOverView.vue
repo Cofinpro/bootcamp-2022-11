e08779dd5a98a7ad9bdc80ac4259f986d54f8cd7
MarkusKremer <Markus.Kremer@cofinpro.de>
2022-12-13 14:50:04 +0100

    [FEATURE] Added user table component and view, added mock data.

---
+++ skill-db/frontend/src/views/UserOverView.vue
@@ -0,0 +1,24 @@
+<template>
+  <UserDetails :users="users"></UserDetails>
+</template>
+
+<script>
+import UserDetails from "@/components/UserDetails.vue";
+import {useUserStore} from "@/stores/UserStore";
+export default {
+  name: "UserIOverView",
+  components: {UserDetails},
+  setup() {
+    const userStore = useUserStore();
+    userStore.loadDummyUser();
+    const users = userStore.users;
+    return {
+      users,
+    };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
