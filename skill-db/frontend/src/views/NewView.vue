d17edd1219d67c7c41d6fc0353779e7e05f3226c
theresa.cofinpro <theresa.riesterer@cofinpro.de>
2022-12-19 13:08:54 +0100

    [FEATURE] Added EditComponent

--- skill-db/frontend/src/views/NewView.vue
+++ skill-db/frontend/src/views/NewView.vue
@@ -2,14 +2,26 @@
   <v-container>
     <h1>Neues Profil erstellen</h1>
     <h2>Hier kannst du ein neues Profil anlegen!</h2>
+
+    <EditComponent :details="detail"/>
   </v-container>
 </template>
 
 <script lang="ts">
 
-export default {
-  setup() {
+import {useDetailStore} from "@/stores/DetailStore";
+import EditComponent from "@/components/EditComponent.vue";
 
+export default {
+  components: {EditComponent},
+  setup() {
+    const detailStore = useDetailStore();
+    detailStore.loadDemoDetails();
+    const detail = detailStore.details;
+
+    return {
+      detail
+    }
   }
 }
 
