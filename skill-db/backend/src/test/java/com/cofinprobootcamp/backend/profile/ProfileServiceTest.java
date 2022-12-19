b75a7170c4b6d4cceca36e5bdac03cb621375ee6
MarkusKremer <Markus.Kremer@cofinpro.de>
2022-12-19 14:24:23 +0100

    [FIX] Unit test for saveProfile funtion of profileservice. Does not work bc we cant mock statics

--- skill-db/backend/src/test/java/com/cofinprobootcamp/backend/profile/ProfileServiceTest.java
+++ skill-db/backend/src/test/java/com/cofinprobootcamp/backend/profile/ProfileServiceTest.java
@@ -12,9 +12,7 @@ import com.cofinprobootcamp.backend.user.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -25,6 +23,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(SpringExtension.class)
 class ProfileServiceTest {
@@ -57,7 +57,35 @@ class ProfileServiceTest {
                 .build();
     }
 
-
+ /*
+    Test does not compile because spring-boot starter test does not inlude Static mocks!
+    @Test
+    void createProfile() {
+        ProfileCreateInDTO profileCreateInDTO = new ProfileCreateInDTO(
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                List.of(""),
+                "",
+                LocalDate.of(2020,10,10)
+        );
+        Set<Skill> skills = Set.of(new Skill(""));
+        User user = new User();
+        Mockito.when(skillService.findSkillIfExistsElseCreateSkill(profileCreateInDTO.skills()))
+                .thenReturn(skills);
+        MockedStatic<ProfileDirector> mockDirector = Mockito.mockStatic(ProfileDirector.class);
+        mockDirector.when(() -> ProfileDirector.CreateInDTOToEntity(profileCreateInDTO, user, skills))
+                        .thenReturn(profile);
+        ArgumentCaptor<Profile> profileArgumentCaptor = ArgumentCaptor.forClass(Profile.class);
+        profileService.createProfile(profileCreateInDTO,user);
+        Mockito.verify(profileRepository.save(profileArgumentCaptor.capture()), times(1));
+        assertThat(profileArgumentCaptor.getValue()).isEqualTo(profile);
+    }
+    */
 
     @Test
     void getProfileById() {
