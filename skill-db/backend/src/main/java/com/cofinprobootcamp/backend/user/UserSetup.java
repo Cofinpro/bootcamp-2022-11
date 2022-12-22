a263ed681f27a52d804da759088c28735991d953
l-rehm <117639138+l-rehm@users.noreply.github.com>
2022-12-22 09:50:52 +0100

    [FEATURE] Added auto user-setup on start

---
+++ skill-db/backend/src/main/java/com/cofinprobootcamp/backend/user/UserSetup.java
@@ -0,0 +1,66 @@
+package com.cofinprobootcamp.backend.user;
+
+import com.cofinprobootcamp.backend.enums.StandardRoles;
+import com.cofinprobootcamp.backend.role.Role;
+import com.cofinprobootcamp.backend.role.RoleService;
+import com.cofinprobootcamp.backend.user.dto.UserCreateInDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class UserSetup {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public UserSetup(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @PostConstruct
+    public void init() {
+        initTestUsers();
+    }
+
+    private void initTestUsers() {
+        List<UserCreateInDTO> testUsers = new LinkedList<>();
+        testUsers.add(dtoFromMinimalInfo(
+                "lennart.rehmer",
+                "ADMIN"));
+        testUsers.add(dtoFromMinimalInfo(
+                "markus.kremer",
+                "USER"));
+        testUsers.add(dtoFromMinimalInfo(
+                "luis.geyer",
+                "USER"));
+        testUsers.add(dtoFromMinimalInfo(
+                "thorben.dreier",
+                "USER"));
+        testUsers.add(dtoFromMinimalInfo(
+                "theresa.riesterer",
+                "USER"));
+
+        testUsers.forEach(this::createOneTestUser);
+    }
+
+    private void createOneTestUser(UserCreateInDTO inDTO) {
+        Optional<Role> roleOptional = roleService.getRoleByName(inDTO.userRole());
+        if (roleOptional.isPresent()) {
+            userService.createUser(inDTO, roleOptional.get());
+        } else {
+            Role role = StandardRoles.fromShortName(inDTO.userRole()).createNewRoleEntity();
+            roleService.saveRole(role);
+            userService.createUser(inDTO, role);
+        }
+    }
+
+    private UserCreateInDTO dtoFromMinimalInfo(String mailBeforeAt, String role) {
+        return new UserCreateInDTO(mailBeforeAt + "@cofinpro.de", "mega_gutes_passwort1", role);
+    }
+}
