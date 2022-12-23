92229d66b4a3bb5d64a68c4a4bb7a788867d1441
MarkusKremer <Markus.Kremer@cofinpro.de>
2022-12-23 16:54:05 +0100

    [FEATURE] Finished Integration Tests for profile endpoint

---
+++ skill-db/backend/src/test/java/com/cofinprobootcamp/backend/user/UserIntegrationTest.java
@@ -0,0 +1,29 @@
+package com.cofinprobootcamp.backend.user;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class UserIntegrationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
