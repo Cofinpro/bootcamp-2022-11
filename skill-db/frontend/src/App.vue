3d37d434221adea0fd2c139c939e3252052fe54b
MarkusKremer <Markus.Kremer@cofinpro.de>
2022-12-27 15:14:08 +0100

    [FEATURE] Added snackbar in app.vue

--- skill-db/frontend/src/App.vue
+++ skill-db/frontend/src/App.vue
@@ -3,6 +3,7 @@
     <SideBar v-if="$route.path !== '/login'"></SideBar>
     <v-main>
       <router-view/>
+      <ErrorSnackbar></ErrorSnackbar>
     </v-main>
   </v-app>
 </template>
@@ -10,8 +11,9 @@
 <script lang="ts">
 import SideBar from './components/SideBar.vue'
 import router from "@/router";
+import ErrorSnackbar from "@/components/ErrorSnackbar.vue";
 export default {
-  components: {SideBar},
+  components: {ErrorSnackbar, SideBar},
   watch: {
     '$route' (to, from) {
       document.title = to.meta.title || 'Website'
