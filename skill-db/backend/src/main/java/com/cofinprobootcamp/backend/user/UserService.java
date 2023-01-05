9d58f0266eef56fbafcf6d1cb6ec765ccaa1d280
l-rehm <117639138+l-rehm@users.noreply.github.com>
2023-01-05 10:22:47 +0100

    [UNFIX] Merged into broken version

--- skill-db/backend/src/main/java/com/cofinprobootcamp/backend/user/UserService.java
+++ skill-db/backend/src/main/java/com/cofinprobootcamp/backend/user/UserService.java
@@ -10,7 +10,6 @@ import com.cofinprobootcamp.backend.user.dto.UserCreateInDTO;
 import com.cofinprobootcamp.backend.user.dto.UserOutDTO;
 import com.cofinprobootcamp.backend.utils.RandomStringGenerator;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
