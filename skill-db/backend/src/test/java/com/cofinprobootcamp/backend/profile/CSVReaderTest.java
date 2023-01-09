934564dc0d455d5ec10fe4e89c349547106a5461
MarkusKremer <Markus.Kremer@cofinpro.de>
2023-01-09 10:50:19 +0100

    [FIX] Added unit test for csv import sad path

--- skill-db/backend/src/test/java/com/cofinprobootcamp/backend/profile/CSVReaderTest.java
+++ skill-db/backend/src/test/java/com/cofinprobootcamp/backend/profile/CSVReaderTest.java
@@ -1,5 +1,6 @@
 package com.cofinprobootcamp.backend.profile;
 
+import com.cofinprobootcamp.backend.exceptions.CSVArgumentNotValidException;
 import com.cofinprobootcamp.backend.exceptions.JobTitleNotFoundException;
 import com.cofinprobootcamp.backend.profile.dto.ProfileCreateInDTO;
 import com.cofinprobootcamp.backend.user.User;
@@ -17,6 +18,7 @@ import java.io.IOException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CSVReaderTest {
     @Mock
@@ -33,7 +35,7 @@ class CSVReaderTest {
     }
     @Test
     @DisplayName("Happy path for reading Single profile Unit test")
-    void readProfileFromFile() throws IOException, JobTitleNotFoundException {
+    void readProfileFromFileHappyPath() throws IOException, JobTitleNotFoundException {
         String email = "markus.kremer@cofinpro.de";
         String name = "Markus";
         String surname = "Kremer";
@@ -67,5 +69,28 @@ class CSVReaderTest {
         assertThat(dtoArgumentCaptor.getValue().phoneNumber()).isEqualTo(phoneNumber);
         assertThat(dtoArgumentCaptor.getValue().birthDate()).isEqualTo(birthdate);
         assertThat(userArgumentCaptor.getValue()).isEqualTo(user);
+    }
+    @Test
+    @DisplayName("Error path for reading Single profile Unit test")
+    void readProfileFromFileSadPath() throws IOException, JobTitleNotFoundException {
+        String email = "markus.kremer@cofinpro.de";
+        String name = "Markus";
+        String surname = "";
+        String jobTitle = "Consultant";
+        String degree = "M.Sc";
+        String primary = "Technologie";
+        String reference = "ref";
+        String skills = "skill1;skill2";
+        String phoneNumber = "111111111111";
+        String birthdate = "1997-10-10";
+        String contentOfFile = "Email,Vorname,Nachname,JobTitel,Abschluss,Primärkompetenz,Referenzen,Skills,Telefonnummer,Geburtsdatum\n" +
+                email + "," + name + "," + surname + "," + jobTitle + "," + degree + "," + primary + "," + reference + "," + skills + "," + phoneNumber + "," + birthdate;
+        Mockito.when(file.getBytes()).thenReturn(contentOfFile.getBytes());
+        ArgumentCaptor<ProfileCreateInDTO> dtoArgumentCaptor = ArgumentCaptor.forClass(ProfileCreateInDTO.class);
+        ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+        User user = new User();
+        Mockito.when(userService.getUserByUsername(email)).thenReturn(user);
+
+        assertThrows(CSVArgumentNotValidException.class, () ->{csvReader.readProfileFromFile();});
     }
 }
