a91cbc81014c0b0d04b572f54eeaa83d47106e07
MarkusKremer <Markus.Kremer@cofinpro.de>
2023-01-13 12:01:03 +0100

    [FIX] fixed label and update profile function

--- skill-db/frontend/src/views/EditView.vue
+++ skill-db/frontend/src/views/EditView.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container :v-if="!detailStore.loading">
-    <EditComponent :update=true
+    <EditComponent :update="true"
                    :detail="detailStore.details"
                    :old-picture="detailStore.profilePic"/>
   </v-container>
