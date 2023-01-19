4e90b69f40272d3a5f99af0bf8917d2be69dc98f
MarkusKremer <Markus.Kremer@cofinpro.de>
2023-01-19 10:02:20 +0100

    [FIX] vitest runs so lets just switch to jest bc why not

--- skill-db/frontend/src/tests/SkillInput.test.ts
+++ skill-db/frontend/src/tests/SkillInput.test.ts
@@ -3,12 +3,11 @@ import {mount} from "@vue/test-utils";
 import leaveButton from "@/components/EditComponents/LeaveButton.vue";
 import ConfirmButton from "@/components/EditComponents/ConfirmButton.vue";
 import vuetify from "@/plugins/vuetify";
-import {describe, it, expect} from "vitest";
 import skillInput from "@/components/EditComponents/SkillInput.vue";
 import {createTestingPinia} from "@pinia/testing";
 
 describe('SkillInput', () => {
-    it('SkillInput Renders', () => {
+    it('SkillInput Renders', async  () => {
         const wrapper = mount(skillInput,
             {
                 props:{
@@ -18,7 +17,11 @@ describe('SkillInput', () => {
                      plugins: [vuetify, createTestingPinia()]
                 }
             });
-        expect(wrapper.text()).toContain('Skill nicht gefunden?')
-
+        expect(wrapper.text()).toContain('Skill nicht gefunden?');
+        let button = wrapper.findComponent('.v-btn');
+        expect(button.exists()).toBeTruthy();
+        const ax = await button.trigger('click');
+        button = wrapper.findComponent('.v-btn');
+        expect(button.exists()).toBeFalsy();
     });
 });
