b117ae6b28239bc4d3316dc123817dbdcf7d611e
theresa.cofinpro <theresa.riesterer@cofinpro.de>
2023-01-20 14:45:54 +0100

    [FIX] Next try to test component

--- skill-db/frontend/src/tests/UserComponent.test.ts
+++ skill-db/frontend/src/tests/UserComponent.test.ts
@@ -1,37 +1,47 @@
-import {describe, it, expect} from "vitest";
+import {beforeEach, describe, it, expect} from "vitest";
 
 import {mount} from "@vue/test-utils";
 import UserComponent from "@/components/UserComponent.vue";
 import vuetify from "@/plugins/vuetify";
 import {createTestingPinia} from "@pinia/testing";
-import {ConvertToUserModel} from "@/models/UserModel";
-import {ConvertToRoleModel} from "@/models/RoleModel";
+import {useUserStore} from "@/stores/UserStore";
 
 describe('UserComponent',() => {
-    it('Clicking on lock icon', async () => {
-        const wrapper = mount(UserComponent,
+
+    let wrapper = null;
+
+    beforeEach(() => {
+        wrapper = mount(UserComponent,
             {
                 global:
-                    {plugins: [vuetify, createTestingPinia({ stubActions: false })]}
+                    {plugins: [vuetify, createTestingPinia({
+                            createSpy: vitest.fn
+                        })]}
             });
+    });
+
+    it('Clicking on lock icon', async () => {
 
         expect(wrapper.text()).contains('Email');
         expect(wrapper.text()).contains('Rolle');
         expect(wrapper.text()).contains('Sperrstatus');
 
-        const store = wrapper.vm.$data.userStore;
+        /*const store = wrapper.vm.$data.userStore;
         store.users = [ConvertToUserModel.toUserModel({
             email: 'test@test.com',
             role: ConvertToRoleModel.toRole({
                 identifier: 'USER',
                 displayName: 'Nutzer'
             })
-        })];
-        console.log(store.users);
-        console.log(wrapper.text())
+        })];*/
 
-        const icon = wrapper.find('.v-icon');
-        expect(icon.exists()).toBeTruthy();
+        const store = useUserStore();
+        await store.loadUsers();
+        console.log(store.users);
+        console.log(wrapper.text());
+
+        /*const icon = wrapper.find('.v-icon');
+        expect(icon.exists()).toBeTruthy();*/
 
         /*const spy = vi.spyOn(wrapper,'toggleLock');
 
