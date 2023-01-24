468c7690d6f6910f476a2fa42870e22f34235e6b
theresa.cofinpro <theresa.riesterer@cofinpro.de>
2023-01-24 17:05:13 +0100

    [FIX] Refactored, reorganized test and added some

---
+++ skill-db/frontend/src/tests/RoleComponentState.test.ts
@@ -0,0 +1,48 @@
+import {describe, it, expect} from "vitest";
+
+import {mount} from "@vue/test-utils";
+import RoleComponent from "@/components/RoleComponents/RoleComponent.vue";
+import vuetify from "@/plugins/vuetify";
+import {ConvertToRoleModel} from "@/models/RoleModel";
+import {ConvertToUserModel} from "@/models/UserModel";
+import {RoleComponentState} from "@/components/RoleComponents/RoleComponentState";
+import RoleDropdown from "@/components/RoleComponents/RoleDropdown.vue";
+
+describe('RoleComponent',() => {
+
+    let roleState;
+
+    beforeEach(() => {
+        roleState = new RoleComponentState();
+        roleState.role = ConvertToRoleModel.toRoleModel({
+            identifier: '',
+            displayName: 'Admin',
+            description: '',
+            user: [],
+            color: ''
+        });
+        roleState.selectedUsers = [ConvertToUserModel.toUserModel({
+            role: ConvertToRoleModel.toRoleModel({
+                displayName: 'HR'
+            })
+        })];
+    });
+
+    it('DisplayName of role is showing, button is emitting', async () => {
+        const wrapper = mount(RoleDropdown,
+            {
+                props: {
+                    state: roleState
+                },
+                global:
+                    {plugins: [vuetify]}
+            });
+
+        expect(wrapper.text()).toContain("Admin"); //name of role
+        expect(wrapper.text()).toContain("(HR)"); //role of selected user
+
+        const button = wrapper.findComponent('.v-btn');
+        await button.trigger('click');
+        expect(wrapper.emitted('clicked')).toBeTruthy();
+    });
+})
--- skill-db/frontend/src/tests/RoleDropdown.test.ts
+++ skill-db/frontend/src/tests/RoleDropdown.test.ts
@@ -1,82 +1,158 @@
 import {describe, it, expect} from "vitest";
 
-import {mount} from "@vue/test-utils";
-import RoleDropdown from "@/components/RoleComponents/RoleDropdown.vue";
-import vuetify from "@/plugins/vuetify";
 import {ConvertToRoleModel, RoleModel} from "@/models/RoleModel";
-import {ConvertToUserModel, UserModel} from "@/models/UserModel";
+import {ConvertToUserModel} from "@/models/UserModel";
+import {RoleComponentState} from "@/components/RoleComponents/RoleComponentState";
+import axiosInstance from "@/axios";
+import {createTestingPinia} from "@pinia/testing";
+import {useUserStore} from "@/stores/UserStore";
 
+describe('RoleComponentState', () => {
 
-describe('RoleDropdown', () => {
-    it('DisplayName of role is showing, button is emitting', async () => {
-        const wrapper = mount(RoleDropdown,
-            {
-                props: {
-                    role: ConvertToRoleModel.toRoleModel({
-                        identifier: '',
-                        displayName: 'Admin',
-                        description: '',
-                        user: [],
-                        color: ''
-                    }),
-                    selectedUsers: [ConvertToUserModel.toUserModel({
-                        role: ConvertToRoleModel.toRoleModel({
-                            displayName: 'HR'
-                        })
-                    })],
-                    allUsers: [new UserModel()]
-                },
-                global:
-                    {plugins: [vuetify]}
-            });
+    let roleState;
 
-        expect(wrapper.text()).toContain("Admin"); //name of role
-        expect(wrapper.text()).toContain("(HR)"); //role of selected user
+    beforeEach(() => {
+        roleState = new RoleComponentState();
+        roleState.role = ConvertToRoleModel.toRoleModel({
+            identifier: 'ADMIN',
+            displayName: 'Admin'
+        });
+        roleState.selectedUsers = [ConvertToUserModel.toUserModel({
+            id: '0',
+            email: 'test@test.com',
+            role: ConvertToRoleModel.toRoleModel({
+                identifier: 'HR',
+                displayName: 'HR'
+            })
+        })];
+        roleState.allUsers = [ConvertToUserModel.toUserModel({
+            id: '1',
+            email: 'test1@test.com',
+            role: ConvertToRoleModel.toRoleModel({
+                identifier: 'ADMIN',
+                displayName: 'Admin'
+            })
+        }), ConvertToUserModel.toUserModel({
+            id: '2',
+            email: 'test2@test.com',
+            role: ConvertToRoleModel.toRoleModel({
+                identifier: 'HR',
+                displayName: 'HR'
+            })
+        }), ConvertToUserModel.toUserModel({
+            id: '3',
+            email: 'test3@test.com',
+            role: ConvertToRoleModel.toRoleModel({
+                identifier: 'USER',
+                displayName: 'Nutzer'
+            })
+        }), ConvertToUserModel.toUserModel({
+            id: '4',
+            email: 'test4@test.com',
+            role: new RoleModel()
+        })];
+    });
 
-        const button = wrapper.findComponent('.v-btn');
-        await button.trigger('click');
-        expect(wrapper.emitted('clicked')).toBeTruthy();
+    it('loadAllRoles() makes correct axios call', async () => {
+        createTestingPinia();
+        const spyAxios = vi.spyOn(axiosInstance, 'get');
+
+        await roleState.loadAllRoles();
+
+        expect(spyAxios).toBeCalledTimes(1);
+        expect(spyAxios).toBeCalledWith('/api/v1/roles');
     });
 
     it('attachRole() attaches role', async () => {
-        const wrapper = mount(RoleDropdown,
-            {
-                props: {
-                    role: new RoleModel(),
-                    selectedUsers: [new UserModel()],
-                    allUsers: [ConvertToUserModel.toUserModel({
-                        id: '1',
-                        email: 'test1@test.com',
-                        role: ConvertToRoleModel.toRoleModel({
-                            displayName: 'Admin'
-                        })
-                    }), ConvertToUserModel.toUserModel({
-                        id: '2',
-                        email: 'test2@test.com',
-                        role: ConvertToRoleModel.toRoleModel({
-                            displayName: 'HR'
-                        })
-                    }), ConvertToUserModel.toUserModel({
-                        id: '3',
-                        email: 'test3@test.com',
-                        role: ConvertToRoleModel.toRoleModel({
-                            displayName: 'Nutzer'
-                        })
-                    }), ConvertToUserModel.toUserModel({
-                        id: '4',
-                        email: 'test4@test.com',
-                        role: new RoleModel()
-                    })]
-                },
-                global:
-                    {plugins: [vuetify]}
-            });
-        const attached = wrapper.vm.attachRole(wrapper.vm.$props.allUsers);
+        roleState.attachRole();
 
-        expect(attached.length).toBe(4);
-        expect(attached).toContain("test1@test.com (Admin)");
-        expect(attached).toContain("test2@test.com (HR)");
-        expect(attached).toContain("test3@test.com (Nutzer)");
-        expect(attached).toContain("test4@test.com ()");
+        expect(roleState.allUsersWithRole.length).toBe(4);
+        expect(roleState.allUsersWithRole).toContain("test1@test.com (Admin)");
+        expect(roleState.allUsersWithRole).toContain("test2@test.com (HR)");
+        expect(roleState.allUsersWithRole).toContain("test3@test.com (Nutzer)");
+        expect(roleState.allUsersWithRole).toContain("test4@test.com ()");
+        expect(roleState.selectedUsersWithRole).toContain("test@test.com (HR)")
     });
+
+    it('prepareSelectDropdown() with defined role works correctly', async () => {
+        createTestingPinia();
+        const spyUsersWithRole = vi.spyOn(roleState, 'loadUsersWithThisRole');
+        const spyAllUsers = vi.spyOn(roleState, 'loadAllUsers');
+
+        await roleState.prepareSelectDropdown(roleState.role);
+
+        expect(spyUsersWithRole).toBeCalledWith(roleState.role);
+        expect(spyAllUsers).toBeCalled();
+        expect(roleState.edit).toBeTruthy();
+        expect(roleState.role).toBe(roleState.role);
+    });
+
+    it('prepareSelectDropdown() with undefined role works correctly', async () => {
+        createTestingPinia();
+        const role = new RoleModel();
+        const spyUsersWithRole = vi.spyOn(roleState, 'loadUsersWithThisRole');
+        const spyAllUsers = vi.spyOn(roleState, 'loadAllUsers');
+
+        await roleState.prepareSelectDropdown(role);
+
+        expect(spyUsersWithRole).not.toBeCalled();
+        expect(spyAllUsers).toBeCalled();
+        expect(roleState.edit).toBeTruthy();
+        expect(roleState.role).toBe(role);
+    });
+
+    it('loadUsersWithThisRole() calls userStore', async () => {
+        createTestingPinia();
+        const userStore = useUserStore();
+        const spyUsersWithRole = vi.spyOn(userStore, 'loadUsersByRoleId');
+
+        await roleState.loadUsersWithThisRole(roleState.role);
+
+        expect(spyUsersWithRole).toBeCalledWith(roleState.role.getIdentifier());
+    });
+
+    it('loadAllUsers() calls userStore', async () => {
+        createTestingPinia();
+        const userStore = useUserStore();
+        const spyAllUsers = vi.spyOn(userStore, 'loadUsers');
+
+        await roleState.loadAllUsers(roleState.role);
+
+        expect(spyAllUsers).toBeCalled();
+    });
+
+    it('in useSubmit() role is not changed, if not needed', async () => {
+        roleState.selectedUsers = [roleState.allUsers[0]];
+        roleState.attachRole();
+        const spyChangeRole = vitest.spyOn(roleState, 'changeRole');
+
+        await roleState.useSubmit();
+
+        expect(spyChangeRole).not.toBeCalled();
+        expect(roleState.edit).toBeFalsy();
+    });
+
+    it('in useSubmit() role is changed, if needed', async () => {
+        roleState.selectedUsers = [roleState.allUsers[1]];
+        roleState.attachRole();
+        const spyChangeRole = vitest.spyOn(roleState, 'changeRole');
+
+        await roleState.useSubmit();
+        console.log(roleState.selectedUsers);
+
+        expect(spyChangeRole).toBeCalledTimes(1);
+        expect(spyChangeRole).toBeCalledWith('2', 'Admin');
+        expect(roleState.edit).toBeFalsy();
+    });
+
+    it('changeRole() makes correct axios call', async () => {
+        createTestingPinia();
+        const spyAxios = vi.spyOn(axiosInstance, 'patch');
+
+        await roleState.changeRole('0', 'Admin');
+
+        expect(spyAxios).toBeCalledTimes(1);
+        expect(spyAxios).toBeCalledWith('/api/v1/users/0/Admin');
+    });
+
 });
